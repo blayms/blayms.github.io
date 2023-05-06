cc45925202a80060e9ea26f910ce9154423c40f3
BroDadi <brodadi123@gmail.com>
2023-05-06 23:08:53 +0600

    2023 blamysys

--- navigation-bar.js
+++ navigation-bar.js
@@ -1,13 +1,20 @@
-// Create navigation bar for current HTML page
-let navbarElement = document.createElement("nav");
-navbarElement.innerHTML = `
-<a href="index.html"><img src="mea.png" alt="Home"></img></a>
-<a href="other.html">Test</a>
-`;
-document.querySelector("body").appendChild(navbarElement);
+function loadStuff()
+{
+    // Create navigation bar for current HTML page
+    let navbarElement = document.createElement("nav");
+    navbarElement.innerHTML = `
+    <a href="index.html"><img src="mea.png" alt="Home"></img></a>
+    <a href="other.html">Test</a>
+    `;
+    document.querySelector("body").appendChild(navbarElement);
 
-// Import styles for current HTML page
+    // Import styles for current HTML page
+    let style = document.createElement("link");
+    document.querySelector("head").appendChild(style)
+    style.outerHTML = `<link rel="stylesheet" href="navigation-bar-style.css">`;
 
-let style = document.createElement("link");
-document.querySelector("head").appendChild(style)
-style.outerHTML = `<link rel="stylesheet" href="navigation-bar-style.css">`;
+    const currentYear = new Date().getFullYear(); // Get the current year
+    const yearText = document.getElementsByClassName('yeartext')[0]; // Get the <p> element
+    yearText.innerHTML = `© ${currentYear}, Blayms`; // Set the text content to include the current year
+}
+setTimeout(loadStuff, 20)
