7b382f3c1e83cd7500c66eed66d8c0a9a897d14a
Blayms <mfreshyttrr@gmail.com>
2023-05-06 18:26:29 +0300

    nav bar deez

---
+++ navigation-bar.js
@@ -0,0 +1,14 @@
+// Create navigation bar for current HTML page
+let navbarElement = document.createElement("nav");
+navbarElement.innerHTML = `
+<a href="./index/">Home</a>
+
+<a href="./other/">Test</a>
+`;
+document.querySelector("body").appendChild(navbarElement);
+
+// Import styles for current HTML page
+
+let style = document.createElement("link");
+document.querySelector("head").appendChild(style)
+style.outerHTML = `<link rel="stylesheet" href="navigation-bar-style.css">`;
