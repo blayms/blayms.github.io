e10c18cfd19bb48a661eb83ec2165ddb1cf3a6ee
BroDadi <brodadi123@gmail.com>
2023-05-07 13:32:56 +0600

    dropdown thiings changing

--- website-functions.js
+++ website-functions.js
@@ -18,17 +18,29 @@ function loadStuff() {
     style.outerHTML = `<link rel="stylesheet" href="styles-container.css">`;
 
     const currentYear = new Date().getFullYear(); // Get the current year
-    const yearText = document.getElementsByClassName('yeartext')[0]; // Get the <p> element
+    const yearText = document.querySelector('.yeartext'); // Get the <p> element
     yearText.innerHTML = `© ${currentYear}, Blayms`; // Set the text content to include the current year
 
     // Dropdowns
-    const dropdownToggle = document.querySelector('.dropdown-toggle');
-    const dropdownMenu = document.querySelector('.dropdown-menu');
+    const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+    const dropdownMenus = document.querySelectorAll('.dropdown-menu');
 
-    dropdownToggle.addEventListener('click', function () {
-        dropdownMenu.classList.toggle('show');
-        dropdownToggle.children[1].classList.toggle('showed');
-    });
+    // Loop through each dropdown toggle and add click event listener
+    for (let i = 0; i < dropdownToggles.length; i++) {
+        dropdownToggles[i].addEventListener('click', function() {
+            // Loop through each dropdown menu and hide any open menus
+            for (let j = 0; j < dropdownMenus.length; j++) {
+                if (dropdownMenus[j].classList.contains('show') && dropdownMenus[j] !== dropdownMenus[i]) {
+                    dropdownMenus[j].classList.remove('show');
+                    dropdownToggles[j].classList.remove('showed');
+                }
+            }
+            // Toggle the selected dropdown menu
+            dropdownMenus[i].classList.toggle('show');
+            dropdownToggles[i].classList.toggle('showed');
+        });
+    }
+
 
     // Get the modal element
     var modal = document.getElementById("myModal");
@@ -43,7 +55,7 @@ function loadStuff() {
     // Open the modal when the button is clicked
     btn.onclick = function () {
         var csCodeIndex = "C# Hello World";
-        var codeBlock = document.getElementById('code-block');
+        var codeBlock = document.querySelector('#code-block');
         codeBlock.innerHTML = `<p style="color:white;position: fixed;margin-top: -25px;font-weight: bold;">${csCodeIndex}</p>
         <pre><code class="language-csharp">${globalInstance.csharp[csCodeIndex]}</code></pre>`;
         modal.style.display = "block";
